b28a9d2f421eb3b76be3c575b2f75b24746d7d54
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2020-07-29 12:53:33 +0700

    Add files via upload

---
+++ app/api/views.py
@@ -0,0 +1,16 @@
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    flash,
+    abort,
+    redirect,
+    url_for,
+    current_app,
+)
+
+module = Blueprint('entity', __name__)
+
+@module.route('/', methods=['GET'])
+def index():
+    return 'Hello'
