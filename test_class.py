64a1ff9030a2e1a78d853894dce753de508e4632
D076 <dezmond076@gmail.com>
2020-08-01 18:45:00 +0700

    night to master

---
+++ test_class.py
@@ -0,0 +1,18 @@
+import unittest
+from manage import app
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_requests(self):
+        response = self.app.post('/auth/', data = {'login':'test', 'password':'123'})
+        self.assertEqual(response.status_code, 200)
