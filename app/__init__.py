a91a1396df1aa36a92813df2d4584e2e2f62a3dd
a1pha1337 <ichigo432@gmail.com>
2020-08-01 16:35:14 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(os.environ['APP_SETTINGS'])
 
-    # db.init_app(app)
-    # with app.test_request_context():
-    #     db.create_all()
+    db.init_app(app)
+    with app.test_request_context():
+        db.create_all()
 
     if app.debug == True:
         try:
