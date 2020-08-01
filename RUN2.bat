6a8b6d7ded7a09d26c831ffacb4ba166f7dca83f
a1pha1337 <45879854+a1pha1337@users.noreply.github.com>
2020-08-01 17:26:13 +0700

    Delete RUN2.bat

--- RUN2.bat
+++
@@ -1,7 +0,0 @@
-pip install -r requirements.txt
-set APP_SETTINGS=config.DevelopmentConfig
-set DATABASE_URL=sqlite:///test.db
-python manage.py db init
-python manage.py db migrate
-python manage.py db upgrade
-python manage.py runserver
