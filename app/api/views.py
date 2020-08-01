bf611c707699271c15f5fddb9ea4348b340c80b6
D076 <dezmond076@gmail.com>
2020-08-01 13:19:17 +0700

    Update views.py

--- app/api/views.py
+++ app/api/views.py
@@ -13,4 +13,32 @@ module = Blueprint('entity', __name__)
 
 @module.route('/', methods=['GET'])
 def index():
-    return 'Hello'
+    return 'def Index'
+
+@module.route('/auth/', methods=['POST'])
+def auth():
+    return 'def auth'
+
+@module.route('/validate/<token>/', methods=['GET'])
+def validate(token):
+    return f'def validate {token}'
+
+@module.route('/user/', methods=['POST'])
+def userRegister():
+    return 'def userRegister'
+
+@module.route('/user/delete/<token>/', methods=['DELETE'])
+def userDelete(token):
+    return f'def userDelete {token}'
+
+@module.route('/user/info/<token>/', methods=['GET'])
+def userInfoGet(token):
+    return f'def userInfoGet {token}'
+
+@module.route('/user/info/public/<login>/', methods=['GET'])
+def userInfoPublicGet(login):
+    return f'def userInfoPublicGet {login}'
+
+@module.route('/user/info/', methods=['PUT'])
+def userInfoEdit(login):
+    return f'def userInfoEdit'
