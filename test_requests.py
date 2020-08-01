7f93a3056058db7e50d22122f8c4c67321fd49f3
D076 <dezmond076@gmail.com>
2020-08-01 18:04:52 +0700

    Some changes

--- test_requests.py
+++ test_requests.py
@@ -2,5 +2,5 @@ import requests
 
 
 r = requests.post('http://127.0.0.1:5000/auth/', data = {'login':'test', 'password':'123'})
-#r = requests.get('http://127.0.0.1:5000/')
+r = requests.get('http://127.0.0.1:5000/')
 print('Ok')
