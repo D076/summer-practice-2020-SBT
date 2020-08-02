2f50fd0ced604310d34e74bd815e381998bdcf87
D076 <dezmond076@gmail.com>
2020-08-02 16:43:52 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -15,10 +15,20 @@ class Test(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+    # def test_registration(self):
+    #     response = self.app.post('/user/', data={'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'})
+    #     self.assertEqual(response.status_code, 200)
+
+    # def test_authentication(self):
+    #     response = self.app.post('/auth/', data={'login':'test@mail.ru', 'password':'test12345'})
+    #     self.assertEqual(response.status_code, 200)
+
     def test_registration(self):
-        response = self.app.post('/user/', data={'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'})
+        info = {'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'}
+        response = self.app.post('/user/', data=json.dumps(info), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 200)
 
     def test_authentication(self):
-        response = self.app.post('/auth/', data={'login':'test@mail.ru', 'password':'test12345'})
+        info = {'login':'test@mail.ru', 'password':'test12345'}
+        response = self.app.post('/auth/', data=json.dumps(info), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 200)
