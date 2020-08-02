a03ab4def51693c843fd5ea040c8a658d5a34b6d
a1pha1337 <ichigo432@gmail.com>
2020-08-02 16:22:46 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -1,4 +1,5 @@
 import unittest
+from flask import jsonify
 from manage import app
 
 class Test(unittest.TestCase):
@@ -10,14 +11,14 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_page(self):
+    def test_index(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
     def test_registration(self):
-        response = self.app.post('/user/', data={'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'})
+        response = self.app.post('/user/', data=jsonify({'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'}))
         self.assertEqual(response.status_code, 200)
 
-    def test_requests(self):
-        response = self.app.post('/auth/', data={'login':'test@mail.ru', 'password':'test12345'})
+    def test_authentication(self):
+        response = self.app.post('/auth/', data=jsonify({'login':'test@mail.ru', 'password':'test12345'}))
         self.assertEqual(response.status_code, 200)
