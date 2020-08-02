890eced0da22f5acad3f30d56586bc396663744d
D076 <dezmond076@gmail.com>
2020-08-02 16:51:38 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -12,17 +12,9 @@ class Test(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_index(self):
+    def test_0_index(self):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-
-    # def test_registration(self):
-    #     response = self.app.post('/user/', data={'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'})
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_authentication(self):
-    #     response = self.app.post('/auth/', data={'login':'test@mail.ru', 'password':'test12345'})
-    #     self.assertEqual(response.status_code, 200)
 
     def test_1_registration(self):
         info = {'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'}
