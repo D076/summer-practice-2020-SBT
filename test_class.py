96bf5768070a0e961086594f96153aeba513b5e8
D076 <dezmond076@gmail.com>
2020-08-02 17:22:30 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -24,7 +24,7 @@ class Test(unittest.TestCase):
     def test_2_authentication(self):
         info = {'login':'test@mail.ru', 'password':'test12345'}
         response = self.app.post('/auth/', data=json.dumps(info), headers={'Content-Type': 'application/json'})
-        self.token = response
+        self.token = str(response.data)
         self.assertEqual(response.status_code, 200)
 
     def test_3_validation(self):
