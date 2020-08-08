928cd6539775c1060042a8ac55de33fdd81f30e0
a1pha1337 <ichigo432@gmail.com>
2020-08-08 19:13:20 +0700

    Update gatewayconnector.py

--- app/gatewayconnector.py
+++ app/gatewayconnector.py
@@ -9,7 +9,8 @@ class GatewayConnector(Thread):
         self, 
         gateway_ip, 
         gateway_port,
-        service_ip, 
+        service_ip,
+        service_port, 
         service_name, 
         service_version, 
         service_apis
@@ -23,6 +24,7 @@ class GatewayConnector(Thread):
 
         # Service info
         self.service_ip = service_ip
+        self.service_port = service_port
         self.service_name = service_name
         self.service_version = service_version
         self.serivce_apis = service_apis
@@ -44,17 +46,21 @@ class GatewayConnector(Thread):
     # Publish apis to gateway service
     def publish(self):
         data = {
-            'address': self.service_ip,
+            'address': 'http://' + self.service_ip,
+            'port': self.service_port,
             'name_service': self.service_name,
             'version_service': self.service_version,
             'api': self.serivce_apis
         }
 
-        response = requests.post(
-            f'http://{self.gateway_ip}:{self.gateway_port}/gateway/publish',
-            json.dumps(data),
-            headers={'content-type': 'application/json'}
-            )
+        try:
+            response = requests.post(
+                f'http://{self.gateway_ip}:{self.gateway_port}/gateway/publish',
+                json.dumps(data),
+                headers={'content-type': 'application/json'}
+                )
+        except:
+            raise ConnectionError('No connection to gateway')
         
         if response.status_code == 200:
             response_data = response.json()
@@ -66,8 +72,11 @@ class GatewayConnector(Thread):
 
 
     def unpublish(self):
-        response = \
-            requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/unpublish/{self.instance_id}')
+        try:
+            response = \
+                requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/unpublish/{self.instance_id}')
+        except:
+            raise ConnectionError('No connection to gateway')
 
         if response.status_code == 200:
             self.ready_to_ping = False
@@ -78,21 +87,27 @@ class GatewayConnector(Thread):
     # Send ready state to gateway service
     def ready(self):
         if not self.instance_id is None:
-            response = \
-                requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/ready/{self.instance_id}')
+            try:
+                response = \
+                    requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/ready/{self.instance_id}')
+            except:
+                raise ConnectionError('No connection to gateway')
 
             if response.status_code == 200:
                 self.ready_to_ping = True
             else:
                 raise ConnectionError
-
         else:
-            raise RuntimeError
+            raise RuntimeError('Instance ID were not given')
 
 
     def __ping(self):
-        response = \
-            requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/ping/{self.instance_id}')
+        try:
+            response = \
+                requests.get(f'http://{self.gateway_ip}:{self.gateway_port}/gateway/ping/{self.instance_id}')
+        except:
+            self.ready_to_ping = False
+            raise ConnectionError('No connection to gateway')
 
         if response.status_code != 200:
             self.ready_to_ping = False
