c40588ef32c582a389a634282082770ebff0b6e3
a1pha1337 <ichigo432@gmail.com>
2020-08-08 15:27:52 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,10 @@ import sys
 from flask import Flask
 from .database import db
 from app.api.tokenManager import TokenManager
+from app.gatewayconnector import GatewayConnector
 
 token_manager = TokenManager()
-
+gateway_connector = None
 
 def create_app():
     app = Flask(__name__)
@@ -18,6 +19,33 @@ def create_app():
         db.create_all()
 
     if not 'db' in sys.argv:
+        apis = [
+            {
+                'path': '/auth',
+                'api_version': 'v1'
+            },
+            {
+                'path': '/user/{login}/{password}/{name}',
+                'api_version': 'v1'
+            },
+            {
+                'path': '/index',
+                'api_version': 'v1'
+            }
+        ]
+
+        gateway_connector = GatewayConnector('127.0.0.1', 1123, '127.0.0.1', 'Auth service', '0.1.1alpha', apis)
+
+        try:
+            gateway_connector.publish()
+            print('[GATEWAY CONNECTOR] Published')
+            gateway_connector.ready()
+            print('[GATEWAY CONNECTOR] Ready')
+            gateway_connector.start()
+            print('[GATEWAY CONNECTOR] Thread started')
+        except ConnectionError:
+            print('Gateway connection error')
+
         token_manager.start()
 
     if app.debug == True:
