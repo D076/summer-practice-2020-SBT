c4b68bdabaf0e137aaca9484bb8b159499497ec4
a1pha1337 <ichigo432@gmail.com>
2020-08-09 14:03:58 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -2,24 +2,27 @@ import json
 import unittest
 from manage import app
 
-unittest.TestLoader.sortTestMethodsUsing = None
 
 class Test(unittest.TestCase):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
 
+
     def tearDown(self):
         pass
+
 
     def test_0_index(self):
         response = self.app.get('/index', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
+
     def test_1_registration(self):
         info = {'login': 'test@mail.ru', 'password': 'test12345', 'name': 'test'}
         response = self.app.post('/user', data=json.dumps(info), headers={'Content-Type': 'application/json'})
         self.assertEqual(response.status_code, 200)
+
 
     def test_2_auth_valid_logout(self):
         info = {'login':'test@mail.ru', 'password':'test12345'}
@@ -42,6 +45,7 @@ class Test(unittest.TestCase):
         response_valid = self.app.get(f'/validate/{token}', follow_redirects=True)
         self.assertEqual(response_valid.status_code, 404)
 
+
     def test_3_user_info_by_token(self):
         info = {'login':'test@mail.ru', 'password':'test12345'}
 
@@ -58,6 +62,7 @@ class Test(unittest.TestCase):
         user_info = json.loads(response_user_info.data.decode('utf8'))
         self.assertEqual(user_info['login'], 'test@mail.ru')
         self.assertEqual(user_info['name'], 'test')
+
 
     def test_4_user_info_by_login(self):
         # User info by login test
