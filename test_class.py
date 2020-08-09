7855846f7b9bc64a4ea2beac3880e378b707a02b
a1pha1337 <ichigo432@gmail.com>
2020-08-09 12:58:55 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -2,13 +2,13 @@ import json
 import unittest
 from manage import app
 
+unittest.TestLoader.sortTestMethodsUsing = None
 
 class Test(unittest.TestCase):
     def setUp(self):
-        unittest.TestLoader.sortTestMethodsUsing = None
         app.testing = True
         self.app = app.test_client()
-        self.token = ''
+        self.token = None
 
     def tearDown(self):
         pass
@@ -26,33 +26,18 @@ class Test(unittest.TestCase):
         info = {'login':'test@mail.ru', 'password':'test12345'}
         response = self.app.post('/auth', data=json.dumps(info), headers={'Content-Type': 'application/json'})
 
-        # после ' забрать 32 символа
-        temp_token = str(response.data)
-        temp_token = temp_token[temp_token.find("'") + 1: 34]
-        self.token = temp_token
+        self.token = response.data
         self.assertEqual(response.status_code, 200)
 
-    # def test_3_validation(self):
-    #     info = self.token
-    #
-    #     # response = self.app.get('/validate/{info}/', follow_redirects=True)
-    #
-    #     # тест
-    #
-    #     response = self.app.get(f'/validate/{info}/', follow_redirects=True)
-    #     # response = self.app.get('/validate/{}/'.format(info), follow_redirects=True)
-    #     # self.client.get('{0}?username=user1&password=pass1'.format(url_for('get_test_data')))
-    #
-    #     # self.assertEqual(info, response.data)
-    #     # конец
-    #     self.assertEqual(response.status_code, 200)
+    def test_3_validation(self):
+        response = self.app.get(f'/validate/{self.token}/', follow_redirects=True)
+
+        self.assertEqual(response.status_code, 200)
     
-    # def test_4_logout(self):
-    #     info = self.token
-    #     response = self.app.get('/logout/{info}/', follow_redirects=True)
-    #     self.assertEqual(response.status_code, 200)
+    def test_4_logout(self):
+        response = self.app.get(f'/logout/{self.token}/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
     
-    # def test_5_validation(self):
-    #     info = self.token
-    #     response = self.app.get('/validate/{info}/', follow_redirects=True)
-    #     self.assertEqual(response.status_code, 404)
+    def test_5_validation(self):
+        response = self.app.get(f'/validate/{self.token}/', follow_redirects=True)
+        self.assertEqual(response.status_code, 404)
