9a01980028d0f29012fd24d514af8d145f078f26
a1pha1337 <ichigo432@gmail.com>
2020-08-09 13:58:40 +0700

    Update test_class.py

--- test_class.py
+++ test_class.py
@@ -42,3 +42,28 @@ class Test(unittest.TestCase):
         response_valid = self.app.get(f'/validate/{token}', follow_redirects=True)
         self.assertEqual(response_valid.status_code, 404)
 
+    def test_3_user_info_by_token(self):
+        info = {'login':'test@mail.ru', 'password':'test12345'}
+
+        # Auth test
+        response_auth = self.app.post('/auth', data=json.dumps(info), headers={'Content-Type': 'application/json'})
+        self.assertEqual(response_auth.status_code, 200)
+
+        token = response_auth.data.decode('utf8')
+
+        # User info by token test
+        response_user_info = self.app.get(f'/user/info/{token}', follow_redirects=True)
+        self.assertEqual(response_user_info.status_code, 200)
+
+        user_info = json.loads(response_user_info.data.decode('utf8'))
+        self.assertEqual(user_info['login'], 'test@mail.ru')
+        self.assertEqual(user_info['name'], 'test')
+
+    def test_4_user_info_by_login(self):
+        # User info by login test
+        response_user_info = self.app.get(f'/user/info/public/test@mail.ru', follow_redirects=True)
+        self.assertEqual(response_user_info.status_code, 200)
+
+        user_info = json.loads(response_user_info.data.decode('utf8'))
+        self.assertEqual(user_info['login'], 'test@mail.ru')
+        self.assertEqual(user_info['name'], 'test')
